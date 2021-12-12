13a069bda4c867368bcce00f1e72b0ddc7a76902
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-12-12 12:57:29 +0100

    2021 day 12 solution

--- src/2021/aoc2021_day12.erl
+++ src/2021/aoc2021_day12.erl
@@ -14,7 +14,7 @@ info() ->
                 year = 2021,
                 day = 12,
                 name = "Passage Pathing",
-                expected = {4549, -1},
+                expected = {4549, 120535},
                 has_input_file = true}.
 
 -type input_type() :: digraph:graph().
@@ -32,33 +32,31 @@ parse(Binary) ->
     lists:foreach(fun({A, B}) ->
                      digraph:add_vertex(G, A),
                      digraph:add_vertex(G, B),
-                     digraph:add_edge(G, A, B),
-                     digraph:add_edge(G, B, A)
+                     add_edge(G, A, B),
+                     add_edge(G, B, A)
                   end,
                   Edges),
     G.
+
+add_edge(_, _, start) ->
+    ok;
+add_edge(_, 'end', _) ->
+    ok;
+add_edge(G, X, Y) ->
+    digraph:add_edge(G, X, Y).
 
 -spec solve1(Input :: input_type()) -> result_type().
 solve1(G) ->
     length(find_all_paths(G, start, sets:new(), [], [], 0)).
 
 -spec solve2(Input :: input_type()) -> result_type().
-solve2(_G) ->
-    0.
+solve2(G) ->
+    length(find_all_paths2(G, start, sets:new(), [], [], 0)).
 
--if(false).
-
-indented_debugFmt(Depth, Fmt, Args) ->
-    Str = lists:duplicate(Depth * 4, " ") ++ io_lib:format(Fmt, Args),
-    io:format("~s~n", [Str]).
-
--else.
-
-indented_debugFmt(_Depth, _Fmt, _Args) ->
-    ok.
-
--endif.
-
+cave_size(start) ->
+    large;
+cave_size('end') ->
+    large;
 cave_size(Name) ->
     [N | _] = atom_to_list(Name),
     if N >= $A andalso N =< $Z ->
@@ -68,24 +66,17 @@ cave_size(Name) ->
     end.
 
 find_all_paths(G, Node, Visited, CurrentPath, Paths, Depth) ->
-    indented_debugFmt(Depth, "visiting ~p", [Node]),
     CurrentPath0 = [Node | CurrentPath],
-    % ?debugFmt("Current path: ~p, visited = ~p", [lists:reverse(CurrentPath0), sets:to_list(Visited)]),
     Visited0 = sets:add_element(Node, Visited),
     Nbrs = digraph:out_neighbours(G, Node),
     lists:foldl(fun(Nbr, PathsIn) ->
                    case Nbr of
                        'end' ->
                            P = ['end' | CurrentPath0],
-                           indented_debugFmt(Depth + 1,
-                                             "visiting end, found path ~p",
-                                             [lists:reverse(P)]),
                            [P | PathsIn];
                        _ ->
                            case {cave_size(Nbr), sets:is_element(Nbr, Visited0)} of
-                               {small, true} ->
-                                   indented_debugFmt(Depth, "not visiting ~p, already seen", [Nbr]),
-                                   PathsIn;
+                               {small, true} -> PathsIn;
                                _ ->
                                    find_all_paths(G,
                                                   Nbr,
@@ -99,6 +90,54 @@ find_all_paths(G, Node, Visited, CurrentPath, Paths, Depth) ->
                 Paths,
                 Nbrs).
 
+find_all_paths2(_, _, _, _, _, Depth) when Depth > 100 ->
+    throw(stackoverflow);
+find_all_paths2(G, Node, Visited, CurrentPath, Paths, Depth) ->
+    case {Node, cave_size(Node), allow_small_cave_visit(Node, CurrentPath, Depth)} of
+        {'end', _, _} ->
+            P = lists:reverse([Node | CurrentPath]),
+            [P | Paths];
+        {_, small, false} ->
+            Paths;
+        {_, _, _F} ->
+            Visited0 = sets:add_element(Node, Visited),
+            Nbrs = digraph:out_neighbours(G, Node),
+            lists:foldl(fun(Nbr, PathsIn) ->
+                           find_all_paths2(G,
+                                           Nbr,
+                                           Visited0,
+                                           [Node | CurrentPath],
+                                           PathsIn,
+                                           Depth + 1)
+                        end,
+                        Paths,
+                        Nbrs)
+    end.
+
+allow_small_cave_visit(Node, Path, _Depth) ->
+    Map = lists:foldl(fun(N, Acc) ->
+                         case cave_size(N) of
+                             small -> maps:update_with(N, fun(Old) -> Old + 1 end, 1, Acc);
+                             _ -> Acc
+                         end
+                      end,
+                      #{},
+                      Path),
+    case maps:size(Map) of
+        0 ->
+            true;
+        _ ->
+            MaxFreq =
+                lists:max(
+                    maps:values(Map)),
+            case {cave_size(Node), MaxFreq, maps:get(Node, Map, 0)} of
+                {small, MF, F} when MF >= 2 andalso F >= 1 ->
+                    false;
+                _ ->
+                    true
+            end
+    end.
+
 btoa(B) when is_binary(B) ->
     list_to_atom(binary_to_list(B)).
 
@@ -110,5 +149,10 @@ ex1_test() ->
     Binary = <<"start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end\n">>,
     ?assertEqual(10, solve1(parse(Binary))),
     ?assertEqual(36, solve2(parse(Binary))).
+
+% freq_test() ->
+%     ?assertEqual(none, freq([start, 'A', 'end'])),
+%     ?assertEqual({3, a}, freq([a, b, a, c, a, b, d, e])),
+%     ?assertEqual({3, a}, freq([start, a, 'A', b, a, c, a, b, d, e])).
 
 -endif.
