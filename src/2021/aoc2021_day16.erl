8970d10ad6d7a30ed2093cf4555d4b3d0cc0e024
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-12-16 07:35:34 +0100

    2021 day 16 solution

---
+++ src/2021/aoc2021_day16.erl
@@ -0,0 +1,132 @@
+-module(aoc2021_day16).
+
+-behavior(aoc_puzzle).
+
+-export([parse/1, solve/1, info/0]).
+
+-include("aoc_puzzle.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
+-spec info() -> aoc_puzzle().
+info() ->
+    #aoc_puzzle{module = ?MODULE,
+                year = 2021,
+                day = 16,
+                name = "Packet Decoder",
+                expected = {927, 1725277876501},
+                has_input_file = true,
+                use_one_solver_fun = true}.
+
+-type input_type() :: binary().
+-type result_type() :: any().
+
+-spec parse(Binary :: binary()) -> input_type().
+parse(<<>>) ->
+    <<>>;
+parse(<<$\n>>) ->
+    <<>>;
+parse(<<A, B, Binary/binary>>) ->
+    Byte = hex_bytes_to_val(A, B),
+    <<Byte, (parse(Binary))/binary>>.
+
+-spec solve(Input :: input_type()) -> result_type().
+solve(Binary) ->
+    {Packets, _} = parse_packet(Binary),
+    {version_sum(Packets), packet_value(Packets)}.
+
+-spec parse_packet(Bitstring :: bitstring()) -> {Packet :: tuple(), Rest :: bitstring()}.
+parse_packet(<<Version:3, TypeID:3, LiteralValue/bitstring>>) when TypeID =:= 4 ->
+    {Literal, Rest} = parse_literal_value(LiteralValue, <<>>),
+    {{literal, Version, TypeID, bitstring_to_decimal(Literal, 0)}, Rest};
+parse_packet(<<Version:3,
+               TypeID:3,
+               LengthTypeID:1,
+               TotalBitLength:15,
+               Payload:TotalBitLength/bitstring,
+               Rest0/bitstring>>)
+    when LengthTypeID =:= 0 ->
+    Packets = parse_packets(Payload, []),
+    {{op, Version, TypeID, Packets}, Rest0};
+parse_packet(<<Version:3, TypeID:3, LengthTypeID:1, NumSubPackets:11, Payload/bitstring>>)
+    when LengthTypeID =:= 1 ->
+    {Packets, Rest} = parse_n_packets(Payload, NumSubPackets, []),
+    {{op, Version, TypeID, Packets}, Rest}.
+
+parse_packets(<<>>, Acc) ->
+    lists:reverse(Acc);
+parse_packets(Binary, Acc) when is_bitstring(Binary) ->
+    {Packet, Rest} = parse_packet(Binary),
+    parse_packets(Rest, [Packet | Acc]).
+
+parse_n_packets(Rest, 0, Acc) ->
+    {lists:reverse(Acc), Rest};
+parse_n_packets(Binary, N, Acc) ->
+    {Packet, Rest} = parse_packet(Binary),
+    parse_n_packets(Rest, N - 1, [Packet | Acc]).
+
+parse_literal_value(<<Prefix:1, Group:4/bitstring, Rest/bitstring>>, Acc)
+    when Prefix =:= 1 ->
+    parse_literal_value(Rest, <<Acc/bitstring, Group:4/bitstring>>);
+parse_literal_value(<<Prefix:1, Group:4, Rest/bitstring>>, Acc) when Prefix =:= 0 ->
+    {<<Acc/bitstring, Group:4>>, Rest}.
+
+version_sum(Packets) when is_list(Packets) ->
+    lists:sum(
+        lists:map(fun version_sum/1, Packets));
+version_sum({op, Version, _TypeID, Packets}) ->
+    Version + version_sum(Packets);
+version_sum({literal, Version, _TypeID, _Literal}) ->
+    Version.
+
+packet_value({literal, _Version, _TypeID, Literal}) ->
+    Literal;
+packet_value({op, _Version, TypeID, Packets}) when TypeID =:= 0 ->
+    lists:sum(packet_values(Packets));
+packet_value({op, _Version, TypeID, Packets}) when TypeID =:= 1 ->
+    lists:foldl(fun erlang:'*'/2, 1, packet_values(Packets));
+packet_value({op, _Version, TypeID, Packets}) when TypeID =:= 2 ->
+    lists:min(packet_values(Packets));
+packet_value({op, _Version, TypeID, Packets}) when TypeID =:= 3 ->
+    lists:max(packet_values(Packets));
+packet_value({op, _Version, TypeID, [P1, P2]}) when TypeID =:= 5 ->
+    case packet_value(P1) > packet_value(P2) of
+        true ->
+            1;
+        false ->
+            0
+    end;
+packet_value({op, _Version, TypeID, [P1, P2]}) when TypeID =:= 6 ->
+    case packet_value(P1) < packet_value(P2) of
+        true ->
+            1;
+        false ->
+            0
+    end;
+packet_value({op, _Version, TypeID, [P1, P2]}) when TypeID =:= 7 ->
+    case packet_value(P1) == packet_value(P2) of
+        true ->
+            1;
+        false ->
+            0
+    end.
+
+packet_values(Packets) ->
+    lists:map(fun packet_value/1, Packets).
+
+hex_bytes_to_val(A, B) ->
+    Abyte = hex_to_dec(A),
+    Bbyte = hex_to_dec(B),
+    Abyte bsl 4 bor Bbyte.
+
+hex_to_dec(X) when X >= $0 andalso X =< $9 ->
+    X - $0;
+hex_to_dec(X) when X >= $A andalso X =< $F ->
+    10 + (X - $A).
+
+bitstring_to_decimal(<<>>, Acc) ->
+    Acc;
+bitstring_to_decimal(<<1:1, Rest/bitstring>>, Acc) ->
+    bitstring_to_decimal(Rest, Acc bsl 1 bor 1);
+bitstring_to_decimal(<<0:1, Rest/bitstring>>, Acc) ->
+    bitstring_to_decimal(Rest, Acc bsl 1).
