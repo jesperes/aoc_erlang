86e6d2ad128b1545f5e8e888f93746fcb12d772a
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-12-22 18:23:41 +0100

    wip

--- src/2021/aoc2021_day22.erl
+++ src/2021/aoc2021_day22.erl
@@ -42,6 +42,7 @@ parse(Binary) ->
 
 -spec solve1(Input :: input_type()) -> result_type().
 solve1(Input) ->
+    io:format(standard_error, "~p~n", [Input]),
     count_cubes(do_solve(lists:filter(fun({_, {XMin, XMax}, {YMin, YMax}, {ZMin, ZMax}}) ->
                                          ?IS_INIT(XMin, XMax, YMin, YMax, ZMin, ZMax)
                                       end,
@@ -57,33 +58,35 @@ count_cubes([]) ->
     0;
 count_cubes([{off, _, _, _} | Rest]) ->
     count_cubes(Rest);
-count_cubes([{on, {XMin, XMax}, {YMin, YMax}, {ZMin, ZMax}} | Rest]) ->
-    (XMax - XMin) * (YMax - YMin) * (ZMax - ZMin) + count_cubes(Rest).
+count_cubes([{on, {XMin, XMax}, {YMin, YMax}, {ZMin, ZMax}} = Cube | Rest]) ->
+    Count = (XMax + 1 - XMin) * (YMax + 1 - YMin) * (ZMax + 1 - ZMin),
+    io:format(standard_error, "Count: ~p = ~p~n", [Cube, Count]),
+    Count + count_cubes(Rest).
 
-do_solve(Instrs) ->
-    do_solve(tl(Instrs), [hd(Instrs)]).
+do_solve([{on, _, _, _} = First | Instrs]) ->
+    do_solve(Instrs, [First]).
 
 do_solve([], NonOverlapping) ->
     NonOverlapping;
 do_solve([A | Rest], NonOverlapping) ->
     ?assert(not has_overlapping_cuboids(NonOverlapping)),
 
-    io:format(standard_error,
-              "~nChecking ~p (~p remaining) (~p non-overlaps)~n",
-              [A, length(Rest), length(NonOverlapping)]),
-    %% Find all the cuboids in the non-overlapping list which overlap with A
-    {OverlapWithA, NonOverlapWithA} =
-        lists:partition(fun(B) -> are_cuboids_overlapping(A, B) end, NonOverlapping),
+    io:format(standard_error, "--~nCurrent: ~p~n", [A]),
+    io:format(standard_error, "Non-overlapping: ~p~n", [NonOverlapping]),
 
-    io:format(standard_error, "-- overlapping: ~p~n", [OverlapWithA]),
-    io:format(standard_error, "-- non-overlapping: ~p~n", [NonOverlapWithA]),
-
-    %% Split the overlapping ones
     NewNonOverlaps =
-        NonOverlapWithA
-        ++ lists:foldl(fun(B, Acc) -> split_cuboids(A, B) ++ Acc end, [], OverlapWithA),
+        lists:foldl(fun(B, Acc) ->
+                       case split_cuboids(B, A) of
+                           false -> Acc ++ [B];
+                           Split -> Acc ++ Split
+                       end
+                    end,
+                    [],
+                    NonOverlapping),
 
-    io:format(standard_error, "Non-overlaps after splitting~n~p~n", [NewNonOverlaps]),
+    io:format(standard_error, "New non-overlapping: ~p~n", [NewNonOverlaps]),
+
+    ?assert(not has_overlapping_cuboids(NewNonOverlaps)),
 
     do_solve(Rest, NewNonOverlaps).
 
@@ -105,43 +108,75 @@ split_cuboids({OnOffA,
 
     OverlapCuboid = {OnOffB, OverlapXRange, OverlapYRange, OverlapZRange},
 
+    io:format(standard_error, "~nA: ~p~n", [A]),
+    io:format(standard_error, "B: ~p~n", [B]),
+
     case is_valid_cuboid(OverlapCuboid) of
         false ->
-            % If there is no overlap, just return the unmodified cuboids
-            [A, B];
+            false;
         true ->
-            CX = if MinXA < MinXB andalso MaxXA < MaxXB ->
-                        %% A: ..[.||]....
-                        %% B: ....[||.]..
+            CX = if MinXA =< MinXB andalso MaxXA < MaxXB ->
+                        % A: |-----|    ->  |--|
+                        % B:    |-----|             |--|
+                        % +:                    |--|
                         [{OnOffA, {MinXA, OverlapXMin - 1}, YRangeA, ZRangeA},
                          {OnOffB, {OverlapXMax + 1, MaxXB}, YRangeB, ZRangeB}];
+                    MinXB =< MinXA andalso MaxXB < MaxXA ->
+                        % A:    |-----|    ->         |--|
+                        % B: |-----|          |--|
+                        % +:                      |--|
+                        [{OnOffB, {MinXB, OverlapXMin - 1}, YRangeA, ZRangeA},
+                         {OnOffA, {OverlapXMax + 1, MaxXA}, YRangeB, ZRangeB}];
                     MinXA < MinXB andalso MaxXA > MaxXB ->
-                        %% A: ..[.||||.]..
-                        %% B: ....[..]....
+                        % A: |-----------| -> |--|    |--|
+                        % B:    |-----|
+                        % +:                      |--|
                         [{OnOffA, {MinXA, OverlapXMin - 1}, YRangeA, ZRangeA},
-                         {OnOffA, {OverlapXMax + 1, MaxXB}, YRangeB, ZRangeB}];
-                    true ->
-                        [{OnOffA, {MinXB, OverlapXMin - 1}, YRangeA, ZRangeA},
-                         {OnOffB, {OverlapXMax + 1, MaxXA}, YRangeB, ZRangeB}]
+                         {OnOffA, {OverlapXMax + 1, MaxXA}, YRangeA, ZRangeA}];
+                    MinXB < MinXA andalso MaxXB > MaxXA ->
+                        % A:    |-----|    ->
+                        % B: |-----------|    |--|    |--|
+                        % +:                      |--|
+                        [{OnOffB, {MinXB, OverlapXMin - 1}, YRangeB, ZRangeB},
+                         {OnOffB, {OverlapXMax + 1, MaxXB}, YRangeB, ZRangeB}]
                  end,
-
-            CY = if MinYA < OverlapYMin ->
+            CY = if MinYA =< MinYB andalso MaxYA < MaxYB ->
+                        % A overlapping left of B
                         [{OnOffA, OverlapXRange, {MinYA, OverlapYMin - 1}, ZRangeA},
                          {OnOffB, OverlapXRange, {OverlapYMax + 1, MaxYB}, ZRangeB}];
-                    true ->
-                        [{OnOffA, OverlapXRange, {MinYB, OverlapYMin - 1}, ZRangeA},
-                         {OnOffB, OverlapXRange, {OverlapYMax + 1, MaxYA}, ZRangeB}]
+                    MinYB =< MinYA andalso MaxYB < MaxYA ->
+                        % A overlapping right of B
+                        [{OnOffB, OverlapXRange, {MinYB, OverlapYMin - 1}, ZRangeA},
+                         {OnOffA, OverlapXRange, {OverlapYMax + 1, MaxYA}, ZRangeB}];
+                    MinYA < MinYB andalso MaxYA > MaxYB ->
+                        % A encloses B
+                        [{OnOffA, OverlapXRange, {MinYA, OverlapYMin - 1}, YRangeA},
+                         {OnOffA, OverlapXRange, {OverlapYMax + 1, MaxYA}, YRangeA}];
+                    MinYB < MinYA andalso MaxYB > MaxYA ->
+                        % B encloses A,
+                        [{OnOffB, OverlapXRange, {MinYB, OverlapYMin - 1}, YRangeB},
+                         {OnOffB, OverlapXRange, {OverlapYMax + 1, MaxYB}, YRangeB}]
                  end,
-
-            CZ = if MinZA < OverlapZMin ->
+            CZ = if MinZA =< MinZB andalso MaxZA < MaxZB ->
+                        % A left of B
                         [{OnOffA, OverlapXRange, OverlapYRange, {MinZA, OverlapZMin - 1}},
                          {OnOffB, OverlapXRange, OverlapYRange, {OverlapZMax + 1, MaxZB}}];
-                    true ->
-                        [{OnOffA, OverlapXRange, OverlapYRange, {MinZB, OverlapZMin - 1}},
-                         {OnOffB, OverlapXRange, OverlapYRange, {OverlapZMax + 1, MaxZA}}]
+                    MinZB =< MinZA andalso MaxZB < MaxZA ->
+                        % A right of B
+                        [{OnOffB, OverlapXRange, OverlapYRange, {MinZB, OverlapZMin - 1}},
+                         {OnOffA, OverlapXRange, OverlapYRange, {OverlapZMax + 1, MaxZA}}];
+                    MinZA < MinZB andalso MaxZA > MaxZB ->
+                        % A encloses B
+                        [{OnOffA, OverlapXRange, OverlapYRange, {MinZA, OverlapZMin - 1}},
+                         {OnOffA, OverlapXRange, OverlapYRange, {OverlapZMax + 1, MaxZA}}];
+                    MinZB < MinZA andalso MaxZB > MaxZA ->
+                        % B encloses A
+                        [{OnOffB, OverlapXRange, OverlapYRange, {MinZB, OverlapZMin - 1}},
+                         {OnOffB, OverlapXRange, OverlapYRange, {OverlapZMax + 1, MaxZB}}]
                  end,
-
-            lists:filter(fun is_valid_cuboid/1, [OverlapCuboid] ++ CX ++ CY ++ CZ)
+            L = lists:filter(fun is_valid_cuboid/1, CX ++ CY ++ CZ ++ [OverlapCuboid]),
+            ?assertNot(has_overlapping_cuboids(L)),
+            L
     end.
 
 overlap({A, B}, {C, D}) ->
@@ -167,7 +202,7 @@ has_overlapping_cuboids(List) ->
     lists:any(fun({A, B}) ->
                  case are_cuboids_overlapping(A, B) of
                      true ->
-                         % io:format(standard_error, "Overlapping:~n~p~n~p~n", [A, B]),
+                         io:format(standard_error, "*** Overlapping:~n*** ~p~n*** ~p~n", [A, B]),
                          true;
                      _ -> false
                  end
@@ -190,20 +225,23 @@ overlap_test() ->
     %% B: ........[..]..
     ?assertEqual(false, is_valid_range(overlap({-10, 5}, {10, 15}))).
 
-split_cuboids_test() ->
-    Split = split_cuboids({a, {0, 10}, {0, 10}, {0, 10}}, {b, {5, 15}, {5, 15}, {5, 15}}),
-    ?assertNot(has_overlapping_cuboids(Split)),
-    io:format(standard_error, ">> ~p~n", [Split]).
+% split_cuboids_test() ->
+%     Split = split_cuboids({on, {0, 10}, {0, 10}, {0, 10}}, {off, {5, 15}, {5, 15}, {5, 15}}),
+%     Count = count_cubes(Split),
+%     ?assertNot(has_overlapping_cuboids(Split)),
+%     ?assertEqual(1115, Count).
 
-ex1_test() ->
-    Binary =
-        <<"on x=10..12,y=10..12,z=10..12\non x=11..13,y=11..13,z=11..13\noff "
-          "x=9..11,y=9..11,z=9..11\non x=10..10,y=10..10,z=10..10">>,
-    X = solve2(parse(Binary)),
-    ?debugFmt("~n~p", [X]).
+% ex1_test() ->
+%     Binary =
+%         <<"on x=10..12,y=10..12,z=10..12\non x=11..13,y=11..13,z=11..13\noff "
+%           "x=9..11,y=9..11,z=9..11\non x=10..10,y=10..10,z=10..10">>,
+%     X = solve1(parse(Binary)),
+%     ?debugFmt("~n~p", [X]).
 
 solve_test() ->
     ?assertEqual(1115,
-                 solve1([{on, {0, 10}, {0, 10}, {0, 10}}, {off, {5, 15}, {5, 15}, {5, 15}}])).
+                 solve1([{on, {0, 10}, {0, 10}, {0, 10}}, {off, {5, 15}, {5, 15}, {5, 15}}])),
+    ?assertEqual(11 * 11 * 11 - 1,
+                 solve1([{on, {0, 10}, {0, 10}, {0, 10}}, {off, {0, 0}, {0, 0}, {0, 0}}])).
 
 -endif.
